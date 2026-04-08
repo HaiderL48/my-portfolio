a8c63f3008da89d68c0413c816809b701214bc0c
pixelpetals-dev <pixelpetals.dev@gmail.com>
2026-04-08 19:17:23 +0530

    Added Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import SmoothScroll from "@/components/smooth-scroll";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -44,6 +45,7 @@ export default function RootLayout({
           <SmoothScroll />
           {children}
           {process.env.NODE_ENV === "production" && <Analytics />}
+          {process.env.NODE_ENV === "production" && <SpeedInsights />}
         </ThemeProvider>
       </body>
     </html>
